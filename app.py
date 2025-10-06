3cfc533ea9665ff9d3f2d0e0cf96db050501e86d
gabrieldmonteiro <gabrieldmonteiro@live.com>
2025-10-06 02:47:16 -0300

    v1.0

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from functions import run
 
-user_input = input("Digite os IDs separados por vírgula: \n")
-ids = [int(i.strip()) for i in user_input.split(",")]
+while True:
+    user_input = input("Digite os IDs separados por vírgula (ou 0 para sair): \n").strip()
+    
+    if user_input == "0":        
+        break
 
-run(ids)
+    id_strings = [i.strip() for i in user_input.split(",")]
+    if all(i.isdigit() for i in id_strings):
+        ids = [int(i) for i in id_strings]
+        run(ids)
+        print("\n")
+    else:
+        print("Entrada inválida! Digite apenas números separados por vírgula.\n")
